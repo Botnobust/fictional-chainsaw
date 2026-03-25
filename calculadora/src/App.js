d2ccbb2909db68a5b873ff293cf002c59784d18e
Botnobust <nathan.palhano@gmail.com>
2026-03-25 14:55:00 -0300

    Update App.js

--- calculadora/src/App.js
+++ calculadora/src/App.js
@@ -9,21 +9,21 @@ function App() {
 
       <div className="buttons">
         <Button label="AC" />
-        <Button label="7" />
+        <Button label="9" />
         <Button label="8" />
         <Button label="/" />
 
-        <Button label="9" />
-        <Button label="4" />
+        <Button label="7" />
+        <Button label="6" />
         <Button label="5" />
         <Button label="*" />
 
-        <Button label="6" />
-        <Button label="1" />
+        <Button label="4" />
+        <Button label="3" />
         <Button label="2" />
         <Button label="-" />
 
-        <Button label="3" />
+        <Button label="1" />
         <Button label="0" />
         <Button label="." />
         <Button label="+" />
